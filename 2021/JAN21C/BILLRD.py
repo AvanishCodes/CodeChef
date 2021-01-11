b293b4f38a15dd00afca35f994da9849ed9c4eae
Avanish Gupta <ui19co15@iiitsurat.ac.in>
2021-01-11 13:26:41 +0530

    Completed Problem Point of Impact

--- 2021/JAN21C/BILLRD.py
+++ 2021/JAN21C/BILLRD.py
@@ -2,8 +2,8 @@
 # Contest: JAN20C(January Long Chaeelenge)
 
 class point():
-    x=0
-    y=0
+    x = 0
+    y = 0
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -12,14 +12,19 @@ class point():
 for _ in range(int(input())):
     # For each test case:
     n, k, x, y = map(int, input().split())
-    origin = point(x, y)
     # print(origin.x, origin.y)
-    strikes = [origin, point, point, point]
+    strikes = [point, point, point, point]
     if x == y:
-        strikes = [point(5, 5), point(5, 5), point(5, 5), point(5, 5)]
-        print(strikes[k%4].x, strikes[k%4].y)
-        continue
+        origin = point(x, y)
+        strikes = [point(n, n), point(n, n), point(n, n), point(n, n)]
+    # The below code is True when the board starts with 0 and not 1 in both directions
     if x > y:
-        strikes = [point(5, 6 - origin.x), point(6 - origin.x, 5), point(1, origin.x), origin]
-    if x > y:
-        strikes = [point(x + abs(origin.x - 3), 5), point(5, x + abs(origin.x - 3)), point(1, origin.x), origin]
+        origin = point(x-y, 0)
+        strikes = [origin, point(n, n - origin.x), point(n- origin.x, n), point(0, origin.x)]
+    if y > x:
+        origin = point(0, y-x)
+        strikes = [origin, point(n - origin.y, n), point(n, n - origin.y), point(origin.y, 0)]
+    # print(f"New Origin: {origin.x}, {origin.y}")
+    # for strike in strikes:
+    #     print(strike.x, strike.y)
+    print(strikes[(k)%4].x, strikes[(k)%4].y)
--- 2021/JAN21C/DECODEIT.py
+++ 2021/JAN21C/DECODEIT.py
@@ -1,1 +1,25 @@
-DECODEIT.py
+def convertToASCII(code):
+    number = 0
+    number += int(code[0])*8
+    number += int(code[1])*4
+    number += int(code[2])*2
+    number += int(code[3])*1
+    return number + 97
+
+def decode(coded):
+    base = ord('a')
+    asciiEquivalent = convertToASCII(coded)
+    # print(asciiEquivalent)
+    return chr(asciiEquivalent)
+
+
+for _ in range(int(input())):
+    # Number of characters in the string
+    n = int(input())
+    code = input()
+    docodedString = ""
+    for i in range(0, n, 4):
+        toBeDecoded = code[i:i+4]
+        # decodedString = decodedString + decode(toBeDecoded)
+        print(decode(toBeDecoded), sep='', end='')
+    print()
