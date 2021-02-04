bb56aaaf6194eda99f2011fc47d9a86efa5c5f0f
Avanish Gupta <ui19co15@iiitsurat.ac.in>
2021-02-04 19:41:18 +0530

    Removed a useless line

--- 2021/PushBack5/ITGUY78.py
+++ 2021/PushBack5/ITGUY78.py
@@ -3,7 +3,7 @@ for _ in range(int(input())):
     nn = int(n)
     if n%2 == 0:
         nn += 1
-    midNumberOfStars = (nn+1)//2
+    # midNumberOfStars = (nn+1)//2
     for i in range(n):
         numberOfStars = i+1 if i<=(nn//2) else (nn-i)
         # print(numberOfStars)
