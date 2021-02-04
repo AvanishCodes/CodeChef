a45b36681da1ce8e212bca3849b2d58b4f2e145f
Avanish Gupta <ui19co15@iiitsurat.ac.in>
2021-02-04 16:00:38 +0530

    Added pattern 6

---
+++ 2021/PushBack5/ITGUY78.py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+    n = int(input())
+    nn = int(n)
+    if n%2 == 0:
+        nn += 1
+    midNumberOfStars = (nn+1)//2
+    for i in range(n):
+        numberOfStars = i+1 if i<=(nn//2) else (nn-i)
+        # print(numberOfStars)
+        numberOfPounds = n - numberOfStars
+        string = '*'*numberOfStars + '#'*numberOfPounds
+        if(i%2 == 1):
+            string = string[::-1]
+        print(str(string))
+        continue
+    continue
